2899261c705a856c12e31c3a82d257f9781b0078
Eric Tovar <tovareric@gmail.com>
2019-05-10 13:13:05 -0500

    finished code

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
-document.getElementById('stopAuto').addEventListener("input", );
+var sum; // state
+
+//cache
+
+var elOne = document.getElementById("boxOne");
+var elTwo = document.getElementById("boxTwo");
+var getSum = document.querySelector("span");
+
+// listen!
+
+document.querySelector('button').addEventListener('click', function() {
+    sum += parseInt(elOne.value) + parseInt(elTwo.value);
+
+    elOne.value = elTwo.value = '';
+render()
+});
+
+
+// functionsss
+
+
+function init () {
+    sum = 0;
+    
+    render ();
+    
+    
+}
+
+function render () {
+    
+    getSum.textContent =  sum;
+    getSum.style.color = sum >= 0 ? 'green' : 'red';
+    
+    
+}
+
+init ();
+
+
 
 
 
